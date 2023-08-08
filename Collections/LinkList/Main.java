e18922b31edaf2ff159088cf92200a2f5602e1c3
Anup <anupbeniwal96@gmail.com>
2023-08-08 22:56:59 +0530

    Linked list method

--- Collections/LinkList/Main.java
+++ Collections/LinkList/Main.java
@@ -4,15 +4,18 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-    LinkedList<String> cars = new LinkedList<String>();
-    cars.add("Volvo");
-    cars.add("BMW");
-    cars.add("Ford");
-    cars.add("Mazda");
-    cars.add("I20"); 
-    cars.removeLast();
-    System.out.println(cars);
-    System.out.println(cars.getLast());
+        LinkedList<String> cars = new LinkedList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        cars.add("I20");
+        cars.removeLast();
+
+        System.out.println(cars);
+        System.out.println(cars.getLast());
+        cars.set(3, "forchunur");
+        System.out.println("Updated LinkedList: " + cars);
     }
-    
+
 }
