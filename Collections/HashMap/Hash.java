0e39fca95e6ad00f616706558c8727777b3b214b
Anup <anupbeniwal96@gmail.com>
2023-08-14 22:59:05 +0530

    Hash map method

--- Collections/HashMap/Hash.java
+++ Collections/HashMap/Hash.java
@@ -1,6 +1,6 @@
 package Collections.HashMap;
 
- import java.util.HashMap;
+import java.util.HashMap;
 
 public class Hash {
   public static void main(String[] args) {
@@ -9,7 +9,11 @@ public class Hash {
     capitalCities.put("Germany", "Berlin");
     capitalCities.put("Norway", "Oslo");
     capitalCities.put("USA", "Washington DC");
-    System.out.println(capitalCities); 
+    System.out.println(capitalCities);
+    capitalCities.remove("England");
+    System.out.println(capitalCities.size());
+    System.out.println(capitalCities);
+    capitalCities.clear();
+    System.out.println(capitalCities);
   }
 }
-   
